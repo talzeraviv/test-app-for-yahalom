17abebbe027546418a65183eff8414295ea9c97b
Tal Zer-Aviv <106658389+talzeraviv@users.noreply.github.com>
2024-03-07 09:51:25 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello Yahalom 1104</h1>
+      <div>
+        <p>
+          We will deploy this app to Vercel.
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
